9acbee3e6608d0caccff9be149cd8d556fa17d35
shiblu <shiblu.msi@gmail.com>
2024-04-03 14:12:49 +0600

    explore item

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. A provident
+          possimus nihil. Temporibus omnis atque illo, molestias odio, quia quos
+          possimus neque alias natus corrupti dignissimos vitae maxime hic,
+          rerum modi? Atque, temporibus.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+}
